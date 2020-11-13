f67f9e76144d766c9fa519a7a64bb2f15ba9ad11
Crowdbotics <team@crowdbotics.com>
2020-11-13 05:10:43 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,92 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(
+        blank=True,
+        max_length=150,
+    )
+    dgyryrturut = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    drgyeryeyrthyfh = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    ghjfhfhdgdgdg = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    fgdfg = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +100,57 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+
+class Test(models.Model):
+    "Generated Model"
+    jngjhgj = models.BigIntegerField()
+    cgvfbgbhnjnmj = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
--- backend/temp_13_nov_dev_15048/wsgi.py
+++ backend/temp_13_nov_dev_15048/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'temp_13_nov_dev_15048.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "temp_13_nov_dev_15048.settings")
 
 application = get_wsgi_application()
